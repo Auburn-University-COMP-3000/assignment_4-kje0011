3b01f808e11808d2fdfa7a7daeeeb67b87251457
kje0011 <kje0011@auburn.edu>
2021-10-19 11:56:01 -0500

    finished q2

--- q2.cpp
+++ q2.cpp
@@ -8,14 +8,31 @@ class Temperature {
 		       degreesFahrenheit,
 		       degreesCelsius;
 	public:
-		void setTempKelvin(double degreesKelvin) {  }
-		double getTempKelvin() {  }
-		void setTempCelsius() {  }
-		double getTempCelsius() {  } 
-		void setTempFahrenheit() {  }
-		double getTempFahrenheit() { }
+		double kelvin, celcius, fahrenheit;
+		void setTempKelvin() {
+			degreesKelvin = kelvin;
+		}
+		double getTempKelvin() {
+			degreesKelvin = kelvin;
+			return degreesKelvin;
+		}
+		double getTempCelsius() {
+			degreesCelsius = getTempKelvin() - 273.15;
+			return degreesCelsius;
+		} 
+		double getTempFahrenheit() {
+			degreesFahrenheit = degreesCelsius *(9/5.0) + 32;
+			return degreesFahrenheit;
+		}
 };
 
 int main() {
+	Temperature temp;
+
+	cout << "Enter a temperature in Kelvin: ";
+	cin >> temp.kelvin;
+
+	cout << "the temperature in Celcius: " << temp.getTempCelsius() << endl;
+	cout << "The temperature in Fahrenheit: " << temp.getTempFahrenheit() << endl;
 
 }
